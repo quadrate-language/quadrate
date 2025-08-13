b8f5b60a2fcc7f573c152f925637e883f4921e7c
klahr <joachim.klahr@gmail.com>
2025-08-14 00:11:22 +0200

    Prepare for adding source

--- qdapi/qd.go
+++ qdapi/qd.go
@@ -2,35 +2,104 @@ package main
 
 /*
 #include <stdlib.h>
+#include <stdint.h>
 
 typedef struct {
+	uintptr_t handle;;
 } qd_module_t;
 
 typedef struct {
-	int dummy;
+	uintptr_t handle;;
 } qd_context_t;
 
 */
 import "C"
-import "unsafe"
+import (
+	"sync"
+	"unsafe"
+)
+
+type QdContext struct {
+	Modules []QdModule
+}
+
+type QdModule struct {
+	ctx     uintptr
+	Name    string
+	Scripts []string
+}
+
+var (
+	mutex    = &sync.Mutex{}
+	contexts = make(map[*C.qd_context_t]*QdContext)
+)
 
 //export qd_create_context
 func qd_create_context() *C.qd_context_t {
 	p := (*C.qd_context_t)(C.malloc(C.size_t(C.sizeof_qd_context_t)))
+	if p == nil {
+		return nil
+	}
+	ctx := &QdContext{}
+	mutex.Lock()
+	contexts[p] = ctx
+	mutex.Unlock()
 	return p
 }
 
 //export qd_free_context
 func qd_free_context(ctx *C.qd_context_t) {
+	if ctx == nil {
+		return
+	}
+	mutex.Lock()
+	delete(contexts, ctx)
+	mutex.Unlock()
 	C.free(unsafe.Pointer(ctx))
 }
 
 //export qd_build
-func qd_build(ctx *C.qd_context_t) {
+func qd_build(mod *C.qd_module_t) {
 }
 
-//export qd_create_module
-func qd_create_module(ctx *C.qd_context_t, name *C.char) *C.qd_module_t {
-	p := (*C.qd_module_t)(C.malloc(C.size_t(C.sizeof_qd_module_t)))
-	return p
+//export qd_execute
+func qd_execute(ctx *C.qd_context_t, fn *C.char) {
 }
+
+//export qd_get_module
+func qd_get_module(ctx *C.qd_context_t, name *C.char) *C.qd_module_t {
+	if ctx == nil {
+		return nil
+	}
+
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	return nil
+}
+
+//export qd_add_script
+func qd_add_script(mod *C.qd_module_t, script *C.char) {
+	if mod == nil {
+		return
+	}
+
+	mutex.Lock()
+	defer mutex.Unlock()
+}
+
+//export qd_register_function
+func qd_register_function(mod *C.qd_module_t, name *C.char, fn unsafe.Pointer) {
+}
+
+func getContext(p *C.qd_context_t) *QdContext {
+	mutex.Lock()
+	defer mutex.Unlock()
+	if ctx, ok := contexts[p]; ok {
+		return ctx
+	}
+	return nil
+}
+
+//func getModule(p *C.qd_module_t) *QdModule {
+//}
--- qdapi/tests/main.c
+++ qdapi/tests/main.c
@@ -1,8 +1,19 @@
 #include <quadrate/qd.h>
 #include <stdio.h>
 
+void my_func(void) {
+	printf("This is a custom function!\n");
+}
+
 int main(void) {
 	qd_context_t* ctx = qd_create_context();
+
+	qd_module_t* os = qd_get_module(ctx, "os");
+	qd_add_script(os, "fn exit() {}");
+	qd_register_function(os, "my_func", my_func);
+	qd_build(os);
+	qd_execute(ctx, "os::exit");
+
 	qd_free_context(ctx);
 
 	printf("Hello, Quadrate!\n");
