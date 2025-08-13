b5787255f6652e0bb183cbfb55fcae8d02114efb
klahr <joachim.klahr@gmail.com>
2025-08-13 22:46:59 +0200

    Restructure qdapi

--- qdapi/qd.go
+++ qdapi/qd.go
@@ -4,8 +4,12 @@ package main
 #include <stdlib.h>
 
 typedef struct {
+} qd_module_t;
+
+typedef struct {
 	int dummy;
 } qd_context_t;
+
 */
 import "C"
 import "unsafe"
@@ -21,4 +25,12 @@ func qd_free_context(ctx *C.qd_context_t) {
 	C.free(unsafe.Pointer(ctx))
 }
 
-func main() {}
+//export qd_build
+func qd_build(ctx *C.qd_context_t) {
+}
+
+//export qd_create_module
+func qd_create_module(ctx *C.qd_context_t, name *C.char) *C.qd_module_t {
+	p := (*C.qd_module_t)(C.malloc(C.size_t(C.sizeof_qd_module_t)))
+	return p
+}
