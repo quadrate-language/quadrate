b77fd8f94e4b107422e59f32ab68e24ce42c21c0
Joachim Klahr <klahr@r8.rs>
2025-10-14 08:20:37 +0200

    Add exec_result

--- lib/cgen/runtime/include/runtime/runtime.h
+++ lib/cgen/runtime/include/runtime/runtime.h
@@ -1,9 +1,14 @@
 #ifndef QD_RUNTIME_RUNTIME_H
 #define QD_RUNTIME_RUNTIME_H
 
+#include <runtime/context.h>
+#include <runtime/exec_result.h>
+
 #ifndef __cplusplus
 extern "C" {
 #endif
+
+qd_exec_result qd_push(qd_context* ctx);
 
 
 #ifndef __cplusplus
